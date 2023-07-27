9a742cc38b8f31ea452d9f84ce6f088fc5f8ec6c
chris <comp1guru@yahoo.com>
2023-07-27 19:59:11 +0000

    aws and output values

---
+++ aws-output-tf/outputs.tf
@@ -0,0 +1,17 @@
+/* outputs.tf
+   outputs to display */
+
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.app_server.id
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.app_server.public_ip
+}
+
+output "instance_state" {
+  description = "Status of the EC2 instance"
+  value       = aws_instance.app_server.instance_state
+}
